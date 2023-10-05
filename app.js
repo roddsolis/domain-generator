5cad5a3f45ebca99cdb1f00351eb26b61bde27a8
Rodd solis <rodrigo.solis.g@gmail.com>
2023-10-05 09:52:29 -0300

    modificacion al docmuento principal

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let nouns = ["gato", "sombrero", "cuentos", "bigote", "fútbol", "chocolate", "p
 let domains = [".com", ".cl", ".net", ".org", ".es", ".io"]
 
 
-/* for (pro in pronouns) {
+for (pro in pronouns) {
     
     for (adj in adjectives) {
 
@@ -12,13 +12,13 @@ let domains = [".com", ".cl", ".net", ".org", ".es", ".io"]
 
             for (dom in domains) {
 
-                // console.log(`${pronouns[pro]}${adjectives[adj]}${nouns[nns]}${domains[dom]}`);
+                console.log(`${pronouns[pro]}${adjectives[adj]}${nouns[nns]}${domains[dom]}`);
                 
             }
         }
     }
     
-} */
+}
 
-/* console.log(nouns.splice(2,7,'baboso')); */
+
 
